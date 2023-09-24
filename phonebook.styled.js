3b9d5c1ba1d38ba88d4373a8b3e001253ced80c5
oliverplay <116948332+oliverplay@users.noreply.github.com>
2023-09-24 12:53:59 +0300

    Create phonebook.styled.js

---
+++ phonebook.styled.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  margin: 10px;
+`;
+
+export const ContactsTable = styled.div`
+  border: 1px solid black;
+  box-sizing: border-box;
+  width: 300px;
+  padding: 5px;
+`;
+
+export const AddContactBttn = styled.button`
+  padding: 1px 15px;
+  margin-top: 5px;
+  cursor: pointer;
+  background-color: white;
+  border-radius: 5px;
+  font-size: 14px; /* Increased font size for readability */
+  border: 1px solid black;
+`;
+
+export const ContactDetails = styled.p`
+  margin: 5px;
+  font-size: 14px;
+`;
+
+export const ContactsContainer = styled.div`
+  margin: 10px;
+`;
+
+export const FindContact = styled.p`
+  margin-bottom: 5px;
+`;
+
+export const RemoveBttn = styled.button`
+  margin-left: 5px;
+  cursor: pointer;
+  background-color: white;
+  border-radius: 5px;
+  font-size: 14px;
+  padding: 1px 15px;
+  border: 1px solid black;
+`;
